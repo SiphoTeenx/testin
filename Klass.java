2d6247963c6d40116025e1fa05260f03c2aa1512
TeenX <ST10451114@rcconnect.edu.za>
2024-09-28 20:34:42 +0000

    Add files via upload

---
+++ Klass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.klass;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Klass {
+
+    public static void main(String[] args) {
+        //Private Details
+        string name;
+        
+        
+    }
+}
